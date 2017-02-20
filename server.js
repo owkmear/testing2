d21989ecd3ba8bae5497ff8edad5c9715a707618
michael <askmeaboutmyschoollife@gmail.com>
2017-02-20 22:55:37 +0300

    fix deploy

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ var file = new(static.Server)();
 var app = http.createServer(function (req, res) {
 	file.serve(req, res);
 });
-port = Number(process.env.PORT || 3000);
+//port = Number(process.env.PORT || 3000);
+port = Number(3000);
 app.listen(port);
 
 var io = require('socket.io').listen(app);
